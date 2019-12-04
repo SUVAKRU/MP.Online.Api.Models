3e7ff21c4a2ccb911a8a81670ee132102bc76c0e
Santosh Goutha <Santosh.Goutha@chrobinson.com>
2019-12-04 05:38:28 -0600

    Created new classes for customer related Transaction responses

---
+++ src/Map.Online.Api.Models/Responses/Customer/DropLocations.cs
@@ -0,0 +1,17 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map.Online.Api.Models.Responses.Customer
+{
+    public class DropLocations : IHasResponseStatus
+    {
+        public string customerNumber { get; set; }
+        public string locationPath { get; set; }
+        public bool duplicateSourceFile { get; set; }
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+}
---
+++ src/Map.Online.Api.Models/Responses/Customer/TransactionResponse.cs
@@ -0,0 +1,17 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map.Online.Api.Models.Responses.Customer
+{
+    public class TransactionResponse : IHasResponseStatus
+    {
+        public int TransactionId { get; set; }
+        public string Message { get; set; }
+        public List<DropLocations> DropLocations { get; set; }
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+}

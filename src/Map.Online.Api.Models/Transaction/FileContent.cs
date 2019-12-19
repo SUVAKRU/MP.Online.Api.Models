3c8c0ba7225b943e184a448c81b05b94b15b5e88
Santosh Goutha <Santosh.Goutha@chrobinson.com>
2019-12-19 07:50:17 -0600

    Name space change.

--- src/Map.Online.Api.Models/Transaction/FileContent.cs
+++ src/Map.Online.Api.Models/Transaction/FileContent.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Map.Online.Api.Models.Vendor
+namespace Map.Online.Api.Models.Transaction
 {
     public class FileContent
     {
